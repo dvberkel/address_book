b0543f2390a0a110531b9e7f99e5b38e2b662df8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-03-06 21:07:19 +0100

    specify AddressBook: respawning

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -111,4 +111,52 @@ describe("An AddressBook", function(){
 	    assert.equal("test@nowhere", capturer.lastEvent().email);
 	});
     });
+
+    describe("respawning", function(){
+	it("should add persons", function(){
+	    var spawn = new AddressBook("spawn");
+	    spawn.on("addPerson", counter.callback);
+	    var events = [{"type": "addPerson", "context" : "spawn", "person" : "test"}];
+
+	    events.forEach(spawn.redo, spawn);
+
+	    assert.equal(1, counter.summary());
+	});
+
+	it("should add persons", function(){
+	    var spawn = new AddressBook("spawn");
+	    spawn.on("addPerson", counter.callback);
+	    var events = [
+		{"type": "addPerson", "context" : "spawn", "person" : "A"},
+		{"type": "addPerson", "context" : "spawn", "person" : "B"},
+	    ];
+
+	    events.forEach(spawn.redo, spawn);
+
+	    assert.equal(2, counter.summary());
+	});
+
+	it("should add persons when context matches", function(){
+	    var spawn = new AddressBook("spawn");
+	    spawn.on("addPerson", counter.callback);
+	    var events = [{"type": "addPerson", "context" : "other", "person" : "test"}];
+
+	    events.forEach(spawn.redo, spawn);
+
+	    assert.equal(0, counter.summary());
+	});
+
+	it("should add addressess", function(){
+	    var spawn = new AddressBook("spawn");
+	    spawn.on("addAddress", counter.callback);
+	    var events = [
+		{"type": "addPerson", "context" : "spawn", "person" : "test"},
+		{"type": "addAddress", "context" : "spawn", "person" : "test", "email" : "test@nowhere" },
+	    ];
+
+	    events.forEach(spawn.redo, spawn);
+
+	    assert.equal(1, counter.summary());
+	});
+    });
 });
