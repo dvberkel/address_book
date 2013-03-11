d3bd131541635692d49957791fe6b78be95d6277
dvberkel <daan.vanberkel@nedap.com>
2013-03-11 16:16:15 +0100

    respond to events request

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var app = express();
 app.use(express.bodyParser());
 app.use(function(req, res, next) {
     req.repository = application.repository();
+    req.store = application.store();
     next();
 });
 
 app.post("/command", routes.handleCommand);
+app.get("/events", routes.events);
 
 var port = process.env.PORT || 3000;
 app.listen(port);
--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,13 @@ exports.handleCommand = function(req, res){
     handler.handle();
     res.send(200);
 }
+
+var normalize = function(since) {
+    return since ? parseInt(since) : -1;
+}
+
+exports.events = function(req, res){
+    var events = [];
+    req.store.since(normalize(req.query.since), function(event){ events.push(event); });
+    res.json(events);
+}
